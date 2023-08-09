0e7122ef44950f0d8e9cdfc258b02b0fdd7a3977
Michael Davis <mcarsondavis@gmail.com>
2023-08-09 17:36:33 -0500

    Port 'data.dic' from Spylls
    The types are quite small and doable in one shot.
    
    I've ported Flag and FlagSet from prior translations of Nuspell: it
    should be possible to support all flag encodings as u32s. Nuspell
    actually uses u16s which makes it harder to store UTF8 flags, but is
    more efficient in the common case. We can investigate later whether it's
    worth the changes necessary to do that. (We would need to port Nuspell's
    String_Set which would be nice to avoid.)
    
    I considered SlotMap for Dic.words: using the index of the Vec seems a
    little messy compared to SlotMap's nice interface. SlotMap adds overhead
    though and only beats a Vec in cases that we don't care about: deleting
    keys.

--- src/dic.rs
+++ src/dic.rs
@@ -1,4 +1,99 @@
 //! Contents of a dictionary.
 //! This comes from Hunspell `.dic` files.
 
-pub struct Dic;
+use std::collections::HashMap;
+
+use crate::{Capitalization, Flag, FlagSet};
+
+#[derive(Debug, Clone)]
+pub(crate) struct Word {
+    // can we cut down on string storage? intern strings?
+    pub stem: String,
+    pub flags: FlagSet,
+    pub data: HashMap<[char; 2], Vec<String>>,
+    pub capitalization: Capitalization,
+}
+
+impl Word {
+    pub fn alternate_spellings(&self) -> &[String] {
+        self.data
+            .get(&['p', 'h'])
+            .map(|alts| alts.as_slice())
+            .unwrap_or_default()
+    }
+}
+
+#[derive(Clone)]
+pub(crate) struct Dic {
+    pub words: Vec<Word>,
+    pub index: HashMap<String, Vec<usize>>,
+    pub lowercase_index: HashMap<String, Vec<usize>>,
+}
+
+impl std::fmt::Debug for Dic {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Dic")
+            .field("words", &format!("{} entries", self.words.len()))
+            .finish_non_exhaustive()
+    }
+}
+
+impl Dic {
+    /// Create a new Dic with the given `capacity`.
+    ///
+    /// `.dic` files start with a line that gives the exact capacity
+    /// of the wordlist.
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            words: Vec::with_capacity(capacity),
+            index: HashMap::with_capacity(capacity),
+            lowercase_index: HashMap::default(),
+        }
+    }
+
+    /// Return all `Word` instances with the same stem,
+    /// optionally ignoring the casing of the words.
+    pub fn homonyms(&self, stem: &str, ignore_case: bool) -> Option<impl Iterator<Item = &Word>> {
+        let index = if ignore_case {
+            &self.lowercase_index
+        } else {
+            &self.index
+        };
+
+        if !index.contains_key(stem) {
+            return None;
+        }
+
+        Some(index[stem].iter().map(|idx| &self.words[*idx]))
+    }
+
+    /// Query if any or all of the homonyms of the stem contain the given flag.
+    pub fn has_flag(&self, stem: &str, flag: Flag, for_all: bool) -> bool {
+        let mut homonyms = match self.homonyms(stem, false) {
+            Some(homonyms) => homonyms,
+            None => return false,
+        };
+
+        let has_flag = |homonym: &Word| homonym.flags.contains(&flag);
+
+        if for_all {
+            homonyms.all(has_flag)
+        } else {
+            homonyms.any(has_flag)
+        }
+    }
+
+    /// Inserts the word into the dictionary and links any lowercase variants.
+    pub fn insert(&mut self, word: Word, lowercase_variants: Vec<String>) {
+        let index = self.words.len();
+        let stem = word.stem.clone(); // TODO interning could turn this into a Copy
+        self.words.push(word);
+        self.index.entry(stem).or_insert_with(Vec::new).push(index);
+        for variant in lowercase_variants {
+            self.lowercase_index
+                .entry(variant)
+                .or_insert_with(Vec::new)
+                .push(index);
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 mod aff;
 mod dic;
 
@@ -20,4 +22,41 @@ impl Dictionary {
     pub fn suggest(&self, _word: &str) -> Vec<String> {
         unimplemented!()
     }
+}
+
+/// Internal compressed representation of a flag.
+pub(crate) type Flag = u32;
+
+/// The set of all flags on a word.
+#[derive(Debug, Default, Clone)]
+pub(crate) struct FlagSet(Vec<Flag>);
+
+impl From<Vec<Flag>> for FlagSet {
+    fn from(mut flags: Vec<Flag>) -> Self {
+        flags.sort_unstable();
+        flags.dedup();
+        FlagSet(flags)
+    }
+}
+
+impl Deref for FlagSet {
+    type Target = Vec<Flag>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub(crate) enum Capitalization {
+    // Hunspell: "NO"
+    Lower,
+    // Hunspell: "TITLE"
+    Title,
+    // Hunspell: "ALL"
+    Upper,
+    // Hunspell: "HUH"
+    Camel,
+    // Hunspell: "HUHINIT"
+    Pascal,
 }
