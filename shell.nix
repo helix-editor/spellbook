e6e526ad6f9e2dfa6a21dd4bc62867ea6f470f11
Michael Davis <mcarsondavis@gmail.com>
2023-08-13 16:19:12 -0500

    flake: Set the local toolchain to match the MSRV
    Starting with an MSRV of 1.65.0.

--- shell.nix
+++ shell.nix
@@ -1,10 +1,13 @@
 { pkgs, }:
-
+let
+  toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
-    (rust-bin.stable.latest.default.override {
+    (toolchain.override {
       extensions = ["rust-src" "rust-analyzer" "clippy"];
     })
+    cargo-flamegraph
   ];
   RUST_BACKTRACE = "1";
   XDG_DATA_DIRS = with pkgs; lib.makeSearchPath "share" [hunspellDicts.en_US];
