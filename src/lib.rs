ca2da7a05c766c70a892319068a62d63f36609cf
Michael Davis <mcarsondavis@gmail.com>
2024-03-07 11:14:10 -0500

    Add a core Flag type

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,32 @@
 
 // TODO: remove.
 #![allow(dead_code)]
+
+/// Compressed representation of a Flag.
+///
+/// Flags are used as attributes about words. For example a flag might mark a word as forbidden,
+/// or it might prevent that word from being suggested. Words in a dictionary have sets of flags
+/// associated to them that control which prefixes and suffixes apply to them.
+///
+/// For a simple example, consider a line in a dic file with the contents `drink/X`. "drink" has
+/// just one flag: `X`. That `X` flag corresponds to a suffix rule in the en_US dictionary that
+/// allows the "drink" _stem_ in the dictionary to be conjugated as full words like "drinkable."
+///
+/// Dictionaries declare a `FlagType` they will use to express flags. This `Flag` can represent
+/// each of the four types.
+///
+/// * `FlagType::Short`: ASCII 8-bit characters are casted into 16 bits.
+/// * `FlagType::Long`: the first ASCII character occupies the higher 8 bits and the second ASCII
+///   character occupies the lower 8 bits.
+/// * `FlagType::Numeric`: the flag is represented as a 16 bit integer.
+/// * `FlagType::Utf8`: if the flag is fit into two bytes. Hunspell and Nuspell restrict UTF8
+///   flags to UTF8 code-points representable in one or two bytes. Flags are just attributes, so
+///   using symbols, emoji or non-latin alphabets is unnecessary. Languages like `ar` (Arabic)
+///   use the `FlagType::Numeric` encoding for example.
+///
+/// Finally, a flag with a value of zero is not valid for any `FlagType`, so we can safely
+/// represent this as a _non-zero_ u16. Hunspell calls this zero flag "`FLAG_NULL`". Layout
+/// optimizations allow us to represent `Option<Flag>` in 16 bits.
+///
+/// Hunspell uses an `unsigned short` for flags while Nuspell uses a `char16_t`.
+pub(crate) type Flag = core::num::NonZeroU16;
