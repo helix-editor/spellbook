a171572f3e4c5e41e3c064f06aff3cc3f217dd28
Michael Davis <mcarsondavis@gmail.com>
2024-03-16 00:16:57 -0400

    Add an example for checking from the dictionary
    This will be useful as we set out on implementing `Dictionary::check`.
    
    Also it's cool to see the timings of the parser. The new implementation
    is 3-4x faster than the old one.
    
    | profile | HEAD  | spylls branch |
    |---      |---    |---            |
    | debug   | 170ms |         465ms |
    | release |  12ms |          44ms |

---
+++ examples/check.rs
@@ -0,0 +1,36 @@
+use std::time::Instant;
+
+use spellbook::Dictionary;
+
+const EN_US_DIC: &str = include_str!("../vendor/en_US/en_US.dic");
+const EN_US_AFF: &str = include_str!("../vendor/en_US/en_US.aff");
+
+fn main() {
+    let mut args = std::env::args().skip(1);
+    let word = match args.next() {
+        Some(arg) => arg,
+        None => {
+            eprintln!("Usage: check WORD");
+            std::process::exit(1);
+        }
+    };
+
+    let now = Instant::now();
+    let dict =
+        Dictionary::new_with_hasher(EN_US_DIC, EN_US_AFF, ahash::RandomState::new()).unwrap();
+    println!("Compiled the dictionary in {}ms", now.elapsed().as_millis());
+
+    let now = Instant::now();
+    if dict.check(&word) {
+        println!(
+            "\"{word}\" is in the dictionary (checked in {}µs)",
+            now.elapsed().as_micros()
+        );
+    } else {
+        eprintln!(
+            "\"{word}\" is NOT in the dictionary (checked in {}µs)",
+            now.elapsed().as_micros()
+        );
+        std::process::exit(1);
+    }
+}
