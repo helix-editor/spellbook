303b92075bd09e1edc6bc506c21c43c82b1b6c03
Michael Davis <mcarsondavis@gmail.com>
2024-08-17 11:35:26 -0400

    Add a very naive example for checking prose

---
+++ examples/prose.rs
@@ -0,0 +1,45 @@
+use std::time::Instant;
+
+use spellbook::Dictionary;
+
+const EN_US_DIC: &str = include_str!("../vendor/en_US/en_US.dic");
+const EN_US_AFF: &str = include_str!("../vendor/en_US/en_US.aff");
+
+fn main() {
+    if std::env::args().nth(1).is_some() {
+        eprintln!("This example takes no arguments. Pipe in prose through stdin.");
+        std::process::exit(1);
+    }
+    let mut total_words = 0;
+    let mut misspelled = 0;
+
+    let now = Instant::now();
+    let dict =
+        Dictionary::new_with_hasher(EN_US_DIC, EN_US_AFF, ahash::RandomState::new()).unwrap();
+    println!("Compiled the dictionary in {}ms", now.elapsed().as_millis());
+
+    let now = Instant::now();
+    for line in std::io::stdin().lines() {
+        let line = match line {
+            Ok(line) => line,
+            Err(err) => {
+                eprintln!("Failed to read line from stdin: {err}");
+                std::process::exit(1);
+            }
+        };
+        for word in line.split_whitespace().flat_map(|s| s.split("--")) {
+            let word = word.trim_matches(|ch: char| !ch.is_ascii_alphabetic());
+            total_words += 1;
+
+            if !dict.check(word) {
+                eprintln!("* {word}");
+                misspelled += 1;
+            }
+        }
+    }
+
+    println!(
+        "Finished in {}ms: {misspelled}/{total_words} words misspelled.",
+        now.elapsed().as_millis()
+    );
+}
