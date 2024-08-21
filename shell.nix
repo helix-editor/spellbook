73d83fcf7cb0f318b88426b3fb8b6212a708e62a
Michael Davis <mcarsondavis@gmail.com>
2024-08-21 10:28:35 -0400

    shell: Add llvm-tools-preview and cargo-llvm-cov

--- shell.nix
+++ shell.nix
@@ -5,10 +5,11 @@ in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     (toolchain.override {
-      extensions = ["rust-src" "clippy"];
+      extensions = ["rust-src" "clippy" "llvm-tools-preview"];
     })
     rust-analyzer
     cargo-flamegraph
+    cargo-llvm-cov
   ];
   RUST_BACKTRACE = "1";
 }
